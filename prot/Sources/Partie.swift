27a74096f50848ea79d16a1c7075b2b8c802e54f
LouisSoleil <43573845+LouisSoleil@users.noreply.github.com>
2018-12-19 15:11:02 +0100

    ajout de Partie incomplet

--- prot/Sources/Partie.swift
+++ prot/Sources/Partie.swift
@@ -47,3 +47,118 @@ protocol Partie : Sequence {
     // pre : Il s'agit d'un joueur : soit 1, soit 2
     mutating func gagner(joueur : Int)
 }
+// structure de la partie : 
+
+
+struct Partie {
+    private var piece : Piece
+    private var position : [Int]
+    private var joueur : Int
+    //private var pieces : [TPiece] =
+    
+    init(){
+        var j1 : joueur = 1
+        var j2 : joueur = 2
+        var ko1 : piece = TPiece(position : [1,1], nom : Kodoma, partie : Self)
+        var ko2 : piece = TPiece(position : [2,1], nom : Kodoma, partie : Self)
+        var ki1 : piece = TPiece(position : [0,2], nom : Kitsune, partie : Self)
+        var ki2 : piece = TPiece(position : [3,0], nom : Kitsune, partie : Self)
+        var ta1 : piece = TPiece(position : [0,0], nom : Tanuki, partie : Self)
+        var ta2 : piece = TPiece(position : [3,3], nom : Tanuki, partie : Self)
+        var ku1 : piece = TPiece(position : [0,1], nom : Kuropokkuru, partie : Self)
+        var ku2 : piece = TPiece(position : [3,1], nom : Kuropokkuru, partie : Self)
+    }
+    
+    func PartieFini() -> Bool {
+        var fin : Bool = false
+        if ku1.proprietairePiece() == 2 || ku2.proprietairePiece() == 1 {
+            return !fin
+        }
+        else if (self.joueurActif()==2 && ku1.positionPiece()[0] == 3){
+            if changerTour() && (ku1.positionPiece()[0]) == 3{
+                return !fin
+            }
+        }
+        else if (self.joueurActif()==1 && ku2.positionPiece()[0] == 0){
+            if changerTour() && (ku2.positionPiece()[0]) == 0{
+                return !fin
+            }
+        }
+    }
+    
+    mutating func joueurActif(){
+        
+    }
+    
+    mutating func changerTour() {
+        if self.joueurActif()==1{
+            return 2
+        }
+        else {
+            return 1
+        }
+    }
+
+    func pieceAPosition(pos : [Int]) -> Piece?{
+        if ko1.positionPiece() == pos {
+            return ko1
+        }
+        else if ko2.positionPiece() == pos {
+            return ko2
+        }
+        else if ki1.positionPiece() == pos{
+            return ki1
+        }
+        else if ki2.positionPiece() == pos{
+            return ki2
+        }
+        else if ta1.positionPiece() == pos{
+            return ta1
+        }
+        else if ta2.positionPiece() == pos{
+            return ta2
+        }
+        else if ku1.positionPiece() == pos{
+            return ku1
+        }
+        else if ku2.positionPiece() == pos{
+            return ku2
+        }
+        else {
+            return nil
+        }
+    }
+    
+    func Est_libre(pos : [Int]) -> Bool{
+        return self.pieceAPosition(pos) == nil
+    }
+    
+    func derniereLigne(joueur : Int) -> Int{
+        if joueur == 1 {
+            return 3
+        }
+        else {
+            return 0
+        }
+    }
+    
+    mutating func gagner(joueur :Int){
+        if ku1 == 2 {
+            print("le joueur 2 a gagné")
+            self.PartieFini()
+        }
+        if ku2 == 1 {
+            print("le joueur 2 a gagné")
+            self.PartieFini()
+        }
+    }
+    
+    func makeIT() -> TotalPieceIT{
+        return TotalPieceIT(partie : self)
+    }
+    
+    func pieceJIT(joueur : Int)-> PieceJIT{
+        return pieceJIT(joueur, partie : self)
+        }
+        
+}
